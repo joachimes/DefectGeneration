d07dbf32c87abebd5b873358295e633ccf31324f
joachimes <joachimes@gmail.com>
2022-12-07 13:59:58 +0100

    Add dataset for generative modelling

--- data_utils/sepData.py
+++ data_utils/sepData.py
@@ -31,7 +31,7 @@ def copy_set(vis_ids, dirpath, target_dir, to_set='train'):
     target_dir = target_dir.replace("train", to_set)
     os.makedirs(osp.join(target_dir, 'images'), exist_ok=True)
     for vis_id in vis_ids:
-        vis_defects = np.unique(glob(osp.join(dirpath, 'images', f'*vialID{int(vis_id)}*')))
+        vis_defects = np.unique(glob(osp.join(dirpath, 'images', f'*ialID{int(vis_id)}*')))
         for vis_defect in vis_defects:
             shutil.copy(vis_defect, osp.join(target_dir, 'images'))
     
@@ -41,12 +41,13 @@ def split_before_move(datapath, target_path, cam_dict):
         defect_dict = {'CrackAbove':2, 'CrackAtFD': 5, 'ChipSh': 2, 'DIST': 2, 'DPCake': 2, 'FMI': 2, 'FMOV':2, 'SSNPDCake':2, 'ChipB':3, 'CC':5, 'CrackAtFD':5}
         # if 'CnC' in dirpath:
         #     print(f'{dirpath}')
-        if 'images' in dirnames and f'{os.sep}train{os.sep}' in dirpath and not osp.exists(target_dir := osp.join(target_path, dirpath[len(datapath)+1:])):
-            train_split = 0.2
-            val_split = 0.4
+        if 'images' in dirnames and f'{os.sep}train' in dirpath:# and not osp.exists(target_dir := osp.join(target_path, dirpath[len(datapath)+1:])):
+            target_dir = osp.join(target_path, dirpath[len(datapath)+1:])
+            train_split = 0.9
+            val_split = 0.1
             train_val_split = train_split + val_split
-            folder_class = dirpath.split(os.sep)[-4]
-            folder_cam = dirpath.split(os.sep)[-5]
+            folder_class = dirpath.split(os.sep)[-3]
+            folder_cam = dirpath.split(os.sep)[-4]
             vis_ids = np.empty(0)
             
             if folder_class in defect_dict.keys():
@@ -69,18 +70,18 @@ def split_before_move(datapath, target_path, cam_dict):
                 
                 if vis_ids.size < 2:
                     full_img_glob = glob(osp.join(dirpath, 'images', '*'))
-                    vis_ids = np.unique([int(x.split('vialID')[1].split('-')[0]) for x in full_img_glob])
+                    vis_ids = np.unique([int(x.split('ialID')[1].split('-')[0]) for x in full_img_glob])
                     
                 np.random.shuffle(vis_ids)
 
                 vis_train_ids = vis_ids[:int(vis_ids.size*train_split)]
-                copy_set(vis_train_ids, dirpath, target_dir)
+                copy_set(vis_train_ids, dirpath, target_dir, 'synthTrain')
 
                 vis_val_ids = vis_ids[int(vis_ids.size*train_split):int(vis_ids.size*train_val_split)]
-                copy_set(vis_val_ids, dirpath, target_dir, 'val')
+                copy_set(vis_val_ids, dirpath, target_dir, 'synthVal')
 
-                vis_test_ids = vis_ids[int(vis_ids.size*train_val_split):]
-                copy_set(vis_test_ids, dirpath, target_dir, 'test')
+                # vis_test_ids = vis_ids[int(vis_ids.size*train_val_split):]
+                # copy_set(vis_test_ids, dirpath, target_dir, 'test')
                 
                 
 
@@ -90,12 +91,14 @@ def split_before_move(datapath, target_path, cam_dict):
 # ChipSH: CAM2
 
 if __name__ == '__main__':
-    data_path = '/nn-seidenader-gentofte/Data/Real'
+    # data_path = '/nn-seidenader-gentofte/Data/Real'
+    data_path = '/nn-seidenader-gentofte/TJSD/VisData/Synthetic/CAM2'
 
-    target_path = '/nn-seidenader-gentofte/tjsd/Data/Real'
+    # target_path = '/nn-seidenader-gentofte/TJSD/VisData/Real'
+    target_path = '/nn-seidenader-gentofte/TJSD/VisData/Synthetic/CAM2'
     defect_cam = np.loadtxt('./data_utils/defect_cam.txt', delimiter=',', dtype=str)
 
-    cam_dict = {}
+    cam_dict = {"Good": 'CAM2'}
     for line in defect_cam:
         cam_dict[line[3]] = line[4]
     split_before_move(data_path, target_path, cam_dict)
--- scripts/vae.sh
+++ scripts/vae.sh
@@ -1,2 +1,2 @@
 cd ../
-python main.py --config-name init_vae state.gpu=-1 dataset.dataset_type=Real_Good
+python main.py --config-name init_vae state.gpu=-1 dataset.dataset_type=Synthetic_generative
